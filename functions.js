5f99cee7a749751580afb36547ec5ddb359bd56d
pilou <p-l.b@hotmail.com>
2023-10-07 14:40:46 +0200

    ended functions guidelines

---
+++ functions.js
@@ -0,0 +1,75 @@
+/**
+ * 1 - Créez une fonction qui prend deux paramètres et renvoie leur somme.
+ */
+
+
+/**
+ * 2 - Créez une fonction qui prend deux paramètres et renvoie leur différence.
+ */
+
+
+/**
+ * 3 - Créez une fonction qui prend deux paramètres et renvoie leur produit.
+ */
+
+/**
+ * 4 - Créez une fonction qui prend deux paramètres et renvoie le résultat de la division du premier par le deuxième. 
+ * Assurez-vous de gérer le cas où le deuxième nombre est zéro.
+ */
+
+
+/**
+ * 5 - Créez une fonction qui prend une chaîne de caractères en paramètre et renvoie le nombre de caractères dans cette chaîne.
+ */
+
+
+/**
+ * 6 - Créez une fonction qui prend une lettre en paramètre et renvoie true si la lettre est majuscule et false si elle est minuscule.
+ */
+
+
+/**
+ * 7 - Créez une fonction qui prend deux chaînes de caractères en paramètre et les concatène, puis renvoie le résultat.
+ */
+
+
+/**
+ * 8 - Créez une fonction qui prend une chaîne de caractères en paramètre et renvoie cette chaîne inversée.
+ */
+
+
+/**
+ * 9 - Créez une fonction qui prend une phrase en paramètre et renvoie le mot le plus long de cette phrase.
+ */
+
+
+/**
+ * 10 - Créez une fonction qui prend un nombre entier positif n en paramètre et renvoie la somme de tous les nombres de 1 à n.
+ */
+
+
+/**
+ * 11 - Créez une fonction qui prend une chaîne de caractères en paramètre et renvoie le nombre de voyelles dans cette chaîne.
+ */
+
+
+/**
+ * 12 - Créez deux fonctions : calculerMoyenne et evaluerMoyenne.
+ *  La première fonction, calculerMoyenne, prend un tableau de nombres en paramètre et retourne la moyenne de ces nombres. 
+ *  La seconde fonction, evaluerMoyenne, prend la moyenne calculée par la première fonction en paramètre et renvoie une évaluation.
+ *  Par exemple, "Passé" si la moyenne est supérieure ou égale à 10, sinon "Échoué".
+ */
+
+const notes = [12, 8, 14, 10, 6];
+
+
+/** 
+ * 13 - Créez deux fonctions : fahrenheitVersCelsius et afficherResultat. 
+ * La première fonction, fahrenheitVersCelsius, prend une température en degrés Fahrenheit en paramètre et retourne la température équivalente en degrés Celsius.
+ *  La seconde fonction, afficherResultat, prend la température en Celsius et l'affiche dans la console avec un message descriptif.
+ * Si la valeur est trop élevée (>= 30), afficher "OMG fuyons !"
+ * Si la valeur est trop basse (<= 10), afficher "Brrrrr couvrons-nous"
+ * Sinon, afficher "Lezgoooo bonne température bb"
+*/
+
+const temp = [32, 70, 100, 20, 50];
---
+++ readme.md
@@ -0,0 +1,3 @@
+### Utilisation
+
+# Pour chaque exercice, faire une branche appelée "nom-exercice/prénom_nom". Exemple : "functions/claire_vannier.
